c2e47847410b6c364444c3bcce193189e66a461e
Brian van Rheenen <b.van.rheenen@bmcr.nl>
2020-09-24 20:46:03 +0200

    Added date and countdown to next payday.

--- cogs/balance.py
+++ cogs/balance.py
@@ -1,4 +1,5 @@
 import discord
+import pendulum
 
 from cogs.maincog import Maincog
 from discord.ext import commands
@@ -7,6 +8,10 @@ class Balance(Maincog):
 
     def __init__(self, client):
         Maincog.__init__(self, client, whitelistedChannels = [756884303580758086])
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.paydayEmoji = self.client.get_emoji(758740464567713823)
 
     @commands.command()
     async def balance(self, ctx):
@@ -36,6 +41,7 @@ class Balance(Maincog):
                     balance = 0
 
                 balanceEmbed = discord.Embed(color=0x3090d9, title="Balance Information")
+                balanceEmbed.description = self._getCountdownNextPayday()
                 balanceEmbed.add_field(name="Current Balance", value=balance)
 
                 await author.send(embed=balanceEmbed)
@@ -43,6 +49,35 @@ class Balance(Maincog):
 
         print(f"Couldn't retrieve balance of {author.display_name}")
         await author.send("Your balance could not be retrieved. Please message someone from Council or Management.")
+
+    @commands.command()
+    async def payday(self, ctx):
+        await ctx.send(self._getCountdownNextPayday())
+
+    def _getCountdownNextPayday(self):
+        timezone = 'Europe/Berlin'
+
+        # Need a starting date to count from - which is 11-9-2020 in this case
+        friday = pendulum.datetime(2020, 9, 11, tz=timezone).next(pendulum.FRIDAY).next(pendulum.FRIDAY)
+        today = pendulum.today(timezone)
+
+        if friday < today:
+            friday = self._addTwoWeeks(friday, today)
+        if friday == today:
+            return f"Payday is **today**! {self.paydayEmoji}"
+
+        now = pendulum.now(timezone)
+        period = pendulum.period(now, friday)
+
+        return f"Next payday: **{friday.format('DD-MM-YYYY')}**, which is in **{period.days} days, {period.hours} hours, {period.minutes} minutes, {period.remaining_seconds} seconds**"
+
+    def _addTwoWeeks(self, datetime, today):
+        datetime = datetime.add(weeks=2)
+
+        if datetime < today:
+            return self._addTwoWeeks(datetime, today)
+        else:
+            return datetime
 
 def setup(client):
     client.add_cog(Balance(client))
